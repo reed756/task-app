c22478415ab3cc5e1be3ceda45b417e9e0856fe8
reed756 <68922323+reed756@users.noreply.github.com>
2021-07-28 18:31:45 +0100

    added props and states so that you can input tasks and they will appear in the task list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import React, { Component } from 'react';
+import Overview from './components/Overview';
+import uniqid from "uniqid";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task: { 
+        text: '',
+        id: uniqid(),
+    },
+      tasks: [],
+    };
+
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      }
+    })
+  }
+  
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { 
+        text: '',
+        id: uniqid()
+      },
+    })
+  }
+
+  render() {
+
+    const { task, tasks } = this.state;
+
+    return (
+      <div className="App">
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter a task</label>
+          <input
+            onChange={this.handleChange}
+            value={task.text}
+            type="text" 
+            id="taskInput"
+          ></input>
+          <button type="submit">Add a task</button>
+        </form>
+        <Overview tasks={tasks} />
+      </div>
+    );
+  }
 }
 
 export default App;
